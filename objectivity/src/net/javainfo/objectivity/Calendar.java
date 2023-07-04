43271f05c7f3bbd4197ead9eaedd73c6d8464ce5
javainfo-net <javainfopage@gmail.com>
2023-07-04 08:47:07 +0200

    add calendar

--- objectivity/src/net/javainfo/objectivity/Calendar.java
+++ objectivity/src/net/javainfo/objectivity/Calendar.java
@@ -1,2 +1,36 @@
-package net.javainfo.objectivity;public class Calendar {
+package net.javainfo.objectivity;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class Calendar {
+
+    public static void main(String[] args){
+
+        LocalDate date = LocalDate.now();
+        int month = date.getMonthValue();
+        int today = date.getDayOfMonth();
+
+        date = date.minusDays(today - 1);
+        DayOfWeek weekday = date.getDayOfWeek();
+        int value = weekday.getValue();
+
+
+        System.out.println(" Pn  Wt  Sr  Cz  Pt  Sb  Nd");
+        for(int i = 1; i < value; ++i)
+            System.out.print("    ");
+        while(date.getMonthValue() == month){
+            System.out.printf("%3d",date.getDayOfMonth());
+            if(date.getDayOfMonth() == today)
+                System.out.print("*");
+            else
+                System.out.print(" ");
+            date = date.plusDays(1);
+            if(date.getDayOfWeek().getValue() == 1)
+                System.out.println();
+        }
+        if(date.getDayOfWeek().getValue() != 1)
+            System.out.println();
+
+    }
 }
